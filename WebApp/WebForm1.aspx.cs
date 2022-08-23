d578fb62857a56ccba30ca5a6bb92f4e1dc29e7a
jromeroGS <111765744+jromeroGS@users.noreply.github.com>
2022-08-23 11:48:29 -0500

    Elimine comentario  casos

--- WebApp/WebForm1.aspx.cs
+++ WebApp/WebForm1.aspx.cs
@@ -162,7 +162,6 @@ namespace WebApp
                     break;
 
                 case "Casos":
-                        //pruebas de casos
                         nombreObjeto = "[Case]";
                         nombrefiltro = "CaseNumber";
                         cadenaSeleccion = "CaseNumber NumeroCaso,Status Estado,Subject Asunto, Description Descripcion, Country_Claim__c PaisReclamacion, Tipo_de_soluci_n__c TipoSolucion, [GROUP].name Propietario";
